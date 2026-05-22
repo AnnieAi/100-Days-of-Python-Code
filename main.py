a644e71a67c6863b4e825fb2f54c49923de18b8b
Annie Ai <129467206+AnnieAi@users.noreply.github.com>
2026-05-22 23:54:11 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-# ??
+from question_model import Question
+# from data import question_data
+from opentdb_data import question_data
+from quiz_brain import QuizBrain
 
-from turtle import Turtle, Screen  # from turtle <module> import Turtle <class>, Screen <class>
+question_bank = []
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("#00ffff")
-tim.color("cyan")
+# data preprocessing
+for dictionary in question_data:
+    ### --- For processing data.py 's question_data --- ###
+    # question_text = dictionary["text"]
+    # question_answer = dictionary["answer"]
+    # new_question = Question(question_text,question_answer)
+
+    # new_question = Question(dictionary["text"], dictionary["answer"])  # Above 3 lines of code just equal to this line
+    # question_bank.append(new_question)
+
+    ### --- For processing opentdb_data.py 's question_data --- ###
+    new_question = Question(dictionary["question"], dictionary["correct_answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
 
 
-for i in range(4):
-    tim.right(90)
-    tim.forward(100)
+# print("You've completed the quiz.")
+# print(f"You final score was: {quiz.score}/{quiz.question_number}")
 
-
-
-screen = Screen()
-screen.exitonclick()
+quiz.show_the_result()
