85de18f9fe832e70a2338c8f1cb7964474aa58ed
Annie Ai <129467206+AnnieAi@users.noreply.github.com>
2026-05-30 23:55:18 +0800

    Add files via upload

---
+++ Task 1. Rollarcoaster.py
@@ -0,0 +1,11 @@
+# Task 1: Whether a child can ride rollercoaster
+
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? "))      # Actually for me, it's more important to understand this.
+
+if height >= 120:
+    print("You can ride the rollercoaster.")
+
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
---
+++ Task 2. Even or odd.py
@@ -0,0 +1,7 @@
+# Task 2: Is the input number an even one or an odd one?
+
+number = int(input("Please input your number: "))
+if number % 2 ==0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
