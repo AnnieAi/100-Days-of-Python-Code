35f7f01164bba9b1a5eac9e6aaece50d9f2a4651
Annie Ai <129467206+AnnieAi@users.noreply.github.com>
2026-06-11 22:35:28 +0800

    Update main.py

--- Day29_Password_Manager/main.py
+++ Day29_Password_Manager/main.py
@@ -1,6 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+    #Password Generator Project
+def generate_password():
+    import random
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    # password = ""
+    # for char in password_list:
+    #   password += char
+
+    print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
@@ -17,8 +46,6 @@ def save():
                 data_file.write(f"{website} | {email} | {password}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -58,7 +85,7 @@ password_entry = Entry()
 password_entry.grid(row=3, column=1, sticky="ew")
 
 # Buttons
-generate_pwd = Button(text="Generate Password")
+generate_pwd = Button(text="Generate Password", command=generate_password)
 generate_pwd.grid(row=3, column=2, sticky="ew")
 
 add = Button(text="Add", command=save)
